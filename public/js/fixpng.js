03ecba146a96dd8b9bb7bb327ddb03b9ff307cfa
Beijing Young Studio <twitter@beijingyoung.com>
2012-04-15 12:59:33 +0800

    .

--- public/js/fixpng.js
+++ public/js/fixpng.js
@@ -1,3 +1,13 @@
+/*
+Correctly handle PNG transparency in Win IE 5.5 & 6.
+http://homepage.ntlworld.com/bobosola. Updated 18-Jan-2006.
+
+Use in <HEAD> with DEFER keyword wrapped in conditional comments:
+<!--[if lt IE 7]>
+<script defer type="text/javascript" src="pngfix.js"></script>
+<![endif]-->
+*/
+
 var DetermineBrowser = navigator.appVersion.split("MSIE");
 var DetermineVersion = parseFloat(DetermineBrowser[1]);
 
